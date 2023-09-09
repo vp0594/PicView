9c36ff3353c019f2158beb5d10fcbeb6a108b05c
vp0594 <vatsalpatel999999@gmail.com>
2023-09-09 21:29:13 +0530

    Tab layout

--- app/src/main/java/com/example/picview/MainActivity.kt
+++ app/src/main/java/com/example/picview/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.picview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var homePageTabLayout: TabLayout
+    private lateinit var homePageViewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        homePageTabLayout = findViewById(R.id.homePageTabLayout)
+        homePageViewPager = findViewById(R.id.homePageViewPage)
+
+        homePageViewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        TabLayoutMediator(homePageTabLayout, homePageViewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "All Photos"
+                1 -> tab.text = "Albums"
+            }
+
+        }.attach()
     }
 }
