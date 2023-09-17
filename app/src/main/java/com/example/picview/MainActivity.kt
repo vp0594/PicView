e62cea14d40267cdffca913f486ce8c3976c13f1
vp0594 <vatsalpatel999999@gmail.com>
2023-09-17 19:19:26 +0530

    Image Permission

--- app/src/main/java/com/example/picview/MainActivity.kt
+++ app/src/main/java/com/example/picview/MainActivity.kt
@@ -1,24 +1,109 @@
 package com.example.picview
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager2.widget.ViewPager2
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.picview.databinding.ActivityMainBinding
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
+    companion object {
+        var hasPermission = false
+        private var first = true
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        checkAppHasPermission()
 
         setContentView(binding.root)
 
         setUpTabLayout()
+    }
+
+
+    private fun checkAppHasPermission() {
+        hasPermission =
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.TIRAMISU)
+                ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            else
+                ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) == PackageManager.PERMISSION_GRANTED
+
+        if (!hasPermission)
+            updateOrRequestPermission()
+    }
+
+
+    private fun updateOrRequestPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.READ_MEDIA_IMAGES),
+                2
+            )
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (requestCode == 2) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasPermission = true
+                } else {
+                    if (!first) {
+                        startActivity(Intent(this@MainActivity, RequestPermission::class.java))
+                        finish()
+                    }
+                    first = false
+                }
+            }
+        } else {
+            if (requestCode == 1) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasPermission = true
+                } else {
+                    if (!first) {
+                        startActivity(Intent(this@MainActivity, RequestPermission::class.java))
+                        finish()
+                    }
+                    first = false
+                }
+            }
+        }
     }
 
     private fun setUpTabLayout() {
@@ -28,7 +113,6 @@ class MainActivity : AppCompatActivity() {
                 0 -> tab.text = "All Photos"
                 1 -> tab.text = "Albums"
             }
-
         }.attach()
     }
 }
---
+++ app/src/main/java/com/example/picview/RequestPermission.kt
@@ -0,0 +1,58 @@
+package com.example.picview
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.example.picview.databinding.ActivityRequestPermissionBinding
+
+class RequestPermission : AppCompatActivity() {
+    private lateinit var binding: ActivityRequestPermissionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRequestPermissionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setPermission()
+    }
+
+    private fun setPermission() {
+        binding.requestPermissionButton.setOnClickListener {
+            startActivity(
+                Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:$packageName")
+                )
+            )
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkHasPermission()
+        if (MainActivity.hasPermission) {
+            startActivity(Intent(this@RequestPermission, MainActivity::class.java))
+        }
+    }
+
+    private fun checkHasPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+            MainActivity.hasPermission = ActivityCompat.checkSelfPermission(
+                this@RequestPermission,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        else
+            MainActivity.hasPermission = ActivityCompat.checkSelfPermission(
+                this@RequestPermission,
+                Manifest.permission.READ_MEDIA_IMAGES
+            ) == PackageManager.PERMISSION_GRANTED
+    }
+
+}
