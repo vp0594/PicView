d9e6b21a9eccd54d04d138f06cc19111aac7c085
vp0594 <vatsalpatel999999@gmail.com>
2023-10-01 20:28:31 +0530

    Completed About

--- app/src/main/java/com/example/picview/About.kt
+++ app/src/main/java/com/example/picview/About.kt
@@ -1,11 +1,22 @@
 package com.example.picview
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.picview.databinding.ActivityAboutBinding
 
 class About : AppCompatActivity() {
+    private lateinit var binding: ActivityAboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gitHubLinkButton.setOnClickListener {
+            val openGithub = Intent(Intent.ACTION_VIEW)
+            openGithub.data = Uri.parse("https://github.com/vp0594/PicView/")
+            startActivity(openGithub)
+        }
     }
 }
